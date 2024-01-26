e51e3e8f740564b42cd5536a8352d04d0c2fed1f
fairleyv <fairleyvincent@gmail.com>
2024-01-26 12:08:12 -0800

    added JavaScript to mark whether an answer was correct or incorrect in first Question

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,15 @@ let question = document.getElementById('question');
 let answers = document.getElementById('answerButtons');
 let starterButton = document.getElementById('starterButton');
 let intro = document.getElementById('introText');
+let footer = document.getElementById('footer')
+// helper Functions for each question
+function question1(event) {
+
+    footer.setAttribute('class', 'border-top');
+    let chosen = document.getElementById('chosen');
+    chosen.textContent = this.value;
+    console.log(this.value)
+}
 
 function startQuiz() {
     // change project heading into question
@@ -24,19 +33,20 @@ function startQuiz() {
     answer2.setAttribute('value', 'incorrect');
     answer3.setAttribute('value', 'correct');
     answer4.setAttribute('value', 'incorrect');
+    // setting id for easier reference
+    answer1.setAttribute('class', 'answer');
+    answer2.setAttribute('class', 'answer');
+    answer3.setAttribute('class', 'answer');
+    answer4.setAttribute('class', 'answer');
     // append answer buttons to DOM
     answers.append(answer1);
     answers.append(answer2);
     answers.append(answer3);
     answers.append(answer4);
+    // add event listener for first question
+    answer1.addEventListener('click', question1)
+    answer2.addEventListener('click', question1)
+    answer3.addEventListener('click', question1)
+    answer4.addEventListener('click', question1)
+
 }
-
-function question1() {
-    if (this.value = 'correct') {
-        
-    } else {
-
-    }
-}
-
-answers.children.addEventListener('click', question1)
